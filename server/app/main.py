2138815578faa62f4b0388c7ecd5b5fac2dcbaed
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2026-07-28 21:23:48 +0545

    register endpoints

--- server/app/main.py
+++ server/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import app.models
-from app.api.v1 import projects, tasks
+from app.api.v1 import ai, projects, tasks
 
 app = FastAPI(title="Bailif Server", version="0.1.0")
 
@@ -16,8 +16,18 @@ app.add_middleware(
 
 # TODO: figure out error handling
 
-app.include_router(tasks.router)
-app.include_router(projects.router)
+app.include_router(
+    tasks.router,
+    prefix="/api/v1",
+)
+app.include_router(
+    projects.router,
+    prefix="/api/v1",
+)
+app.include_router(
+    ai.router,
+    prefix="/api/v1",
+)
 
 
 @app.get("/health")
