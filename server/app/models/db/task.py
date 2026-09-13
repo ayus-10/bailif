12a859867c1daceb82660118d88f75bf294e3f8a
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2026-09-13 22:31:10 +0545

    we will be right back

--- server/app/models/db/task.py
+++ server/app/models/db/task.py
@@ -47,7 +47,7 @@ class Task(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    slug: Mapped[int] = mapped_column(
+    public_id: Mapped[int] = mapped_column(
         BigInteger,
         Identity(start=1000, increment=1),
         unique=True,
--- server/app/models/db/user.py
+++ server/app/models/db/user.py
@@ -27,8 +27,7 @@ class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-
-    slug: Mapped[int] = mapped_column(
+    public_id: Mapped[int] = mapped_column(
         BigInteger,
         Identity(start=1000, increment=1),
         unique=True,
